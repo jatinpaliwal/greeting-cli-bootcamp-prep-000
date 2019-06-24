5f506eab2dfc8fd9b4992acf3ca32640e1f4ef7c
Jatin Paliwal <jatinpaliwal181@gmail.com>
2019-06-24 17:03:10 +0000

    Automatically backed up by Learn

--- lib/greeting.rb
+++ lib/greeting.rb
@@ -1,7 +1,6 @@
 # code the #greeting method here!
 def greeting(name)
-  #name=name.delete(' ')
-  #name=name.delete('\t')
+  name=name.delete('\t')
   name = name.gsub( /\s+/, " " )
   puts "Hello #{name}. It's nice to meet you."
 end
